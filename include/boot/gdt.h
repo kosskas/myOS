7490bc50cc8e2d3f0fb8cff151eb7ec385dc0eb3
kosskas <93159483+kosskas@users.noreply.github.com>
2024-02-14 22:22:38 +0000

    Fix gdt

--- include/boot/gdt.h
+++ include/boot/gdt.h
@@ -10,8 +10,12 @@ typedef struct segment_descriptor{
     uint8_t     base_vhi;
 } __attribute__((packed)) segment_descriptor_t;
 
+typedef struct gdt_ptr{
+    uint16_t    size;
+    uint32_t    base;
+} __attribute__((packed)) gdt_ptr_t;
 
 void create_descriptor(uint32_t num,uint32_t base, uint32_t limit, uint8_t flags);
 void set_gdt();
-void load_gdt(void* gdtr);
+void load_gdt(gdt_ptr_t* gdtr);
 #endif
