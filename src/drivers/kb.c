189a4c57c1c1cd13487482ccd7ef204432eb2a65
kosskas <93159483+kosskas@users.noreply.github.com>
2024-02-16 13:02:17 +0000

    Fix scancode convertion

--- src/drivers/kb.c
+++ src/drivers/kb.c
@@ -1,5 +1,40 @@
 #include <drivers/kb.h>
 
+char kbd_US [128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',   
+  '\t', /* <-- Tab */
+  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',     
+    0, /* <-- control key */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',  0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',   0,
+  '*',
+    0,  /* Alt */
+  ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+  '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+  '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
+
 void activate_kb(){
     uint16_t dataport = 0x60;
     uint16_t cmdport = 0x64;
@@ -24,77 +59,9 @@ uint32_t keyboard_handler(uint32_t stackPtr){
     }
     return stackPtr;
 }
-/*
-    uint8_t key = read8(0x60);
-        switch (key)
-        {
-        //case 0xFA:
-        //case 0x45:
-        //case 0xC5:
-        //    break;
-        
-        default:
-            char* text = "0x00 ";
-            char* hex = "0123456789ABCDEF";
-            text[2] = hex[(key >> 4) & 0x0F];
-            text[3] = hex[key & 0x0F];
-            printf(text);
-            break;
-        }
-
-*/
 char get_char(uint8_t key){
     if(key < 0x80) {
-        switch (key) {
-            case 0x02: return '1';
-            case 0x03: return '2';
-            case 0x04: return '3';
-            case 0x05: return '4';
-            case 0x06: return '5';
-            case 0x07: return '6';
-            case 0x08: return '7';
-            case 0x09: return '8';
-            case 0x0A: return '9';
-            case 0x0B: return '0';
-
-            case 0x10: return 'q';
-            case 0x11: return 'w';
-            case 0x12: return 'e';
-            case 0x13: return 'r';
-            case 0x14: return 't';
-            case 0x15: return 'z';
-            case 0x16: return 'u';
-            case 0x17: return 'i';
-            case 0x18: return 'o';
-            case 0x19: return 'p';
-
-            case 0x1E: return 'a';
-            case 0x1F: return 's';
-            case 0x20: return 'd';
-            case 0x21: return 'f';
-            case 0x22: return 'g';
-            case 0x23: return 'h';
-            case 0x24: return 'j';
-            case 0x25: return 'k';
-            case 0x26: return 'l';
-
-            case 0x2C: return 'y';
-            case 0x2D: return 'x';
-            case 0x2E: return 'c';
-            case 0x2F: return 'v';
-            case 0x30: return 'b';
-            case 0x31: return 'n';
-            case 0x32: return 'm';
-            case 0x33: return ',';
-            case 0x34: return '.';
-            case 0x35: return '-';
-
-            case 0x1C: return '\n';
-            case 0x39: return ' ';
-            
-            default:
-                return key;
-        }
+        return kbd_US[key];
     }
-    return key;
+    return '?';
 }
